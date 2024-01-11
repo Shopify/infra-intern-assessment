0d704dd4018e9a84cd87d60a3bc1ff9cf9826b34
Alex Carvalho <alexander_sck@live.ca>
2024-01-11 10:56:13 -0500

    feat: placement check base implementation

--- sudoku.go
+++ sudoku.go
@@ -1,17 +1,36 @@
 package main
 
+// Alexander Carvalho [01-11-2024]
+//
+//
+
 const G = 9  // Grid dimensions (9x9)
 const SG = 3 // Sub-grid dimensions (3x3)
 
-// isValidPlacement receives a 9x9 sudoku board, row, col, and num, and verifies if the number can replace a 0 on the sudoku board
+// `isValidPlacement` receives a 9x9 sudoku board, row, col, and num, and verifies if the number can replace a 0 on the sudoku board
 func isValidPlacement(board [][]int, row int, col int, num int) bool {
+	// Check if `num` exists in the current row or column
+	for idx := 0; idx < G; idx++ {
+		if board[row][idx] == num || board[idx][col] == num {
+			return false
+		}
+	}
 
-	// logic for validating cell and surrounding cells
+	// Check if `num` exists in the 3x3 sub-grid
+	rowInit, colInit := row-(row%3), col-(col%3) // use modulus to get coordinates of sub-grid start
+	for rowIdx := 0; rowIdx < SG; rowIdx++ {
+		for colIdx := 0; colIdx < SG; colIdx++ {
+			if board[rowInit+rowIdx][colInit+colIdx] == num {
+				return false
+			}
+		}
+	}
 
+	// Can place `num` in this cell
 	return true
 }
 
-// SolveSudoku recieves a 9x9 sudoku board and returns a solved version of the board.
+// `SolveSudoku` recieves a 9x9 sudoku board and returns a solved version of the board.
 func SolveSudoku(board [][]int) [][]int {
 	row, col := 0, 0
 
@@ -23,24 +42,24 @@ func SolveSudoku(board [][]int) [][]int {
 
 		if board[row][col] == 0 {
 			for num := 1; num <= G; num++ {
-				// Validate num placement on board
+				// Validate `num` placement on board
 				if isValidPlacement(board, row, col, num) {
 					board[row][col] = num
 
-					// Return updated board with new num added
+					// Validate XXX
 					if SolveSudoku(board) != nil {
 						return board
 					}
 
-					board[row][col] = 0 // Backtrack num placement if new num XXX
+					board[row][col] = 0 // Backtrack `num`placement if XXX
 				}
 			}
-			return nil // Cannot place num in this cell
+			return nil // Cannot place `num` in this cell
 		}
 
 		col++
 
-		// Move to next row when last column reached
+		// Increment `row` when last `col` reached, reset `col` index
 		if col >= G {
 			col = 0
 			row++
