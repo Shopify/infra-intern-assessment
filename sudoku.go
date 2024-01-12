099bc3a23f113b61deb556943f5f98cf750ef094
ShevinuM <nsmcooray@mun.ca>
2024-01-12 18:50:16 -0330

    Improved comments

--- sudoku.go
+++ sudoku.go
@@ -1,3 +1,33 @@
+/*
+	Explantion to the solution:
+
+	1. I defined a function `tryValuesConcurrently` that takes a Sudoku puzzle and a cell position (row and column) as input.
+
+	2. I created a `WaitGroup` to keep track of all the goroutines that I'm going to start.
+
+	3. I created a buffered channel `solution` that can hold one solution. This is where my goroutines will send the solved puzzle.
+
+	4. I started a loop from 1 to 9. For each number:
+
+	- I incremented the `WaitGroup` counter by 1.
+
+	- I started a goroutine. Inside the goroutine:
+
+		- I made a copy of the puzzle.
+
+		- I checked if the current number is a valid entry for the current cell using the `isPossible` function.
+
+		- If the number is valid, I placed it in the cell and called `SolveSudoku` recursively on the modified puzzle.
+
+		- If `SolveSudoku` found a solution, I sent the solved puzzle to the `solution` channel. I used a `select` statement to ensure that if the channel is already full (a solution has already been found), the goroutine doesn't block and can finish.
+
+		- I decremented the `WaitGroup` counter by 1.
+
+	5. After starting all goroutines, I waited for all of them to finish using `wg.Wait()`. This ensures that the function doesn't return before all goroutines have had a chance to find a solution.
+
+	This way, I used concurrency to speed up the Sudoku solving process by trying all possible numbers for a cell at the same time.
+*/
+
 package main
 
 import (
@@ -24,7 +54,13 @@ func SolveSudoku(puzzle [][]int) [][]int {
 	return puzzle
 }
 
-// tryValuesConcurrently tries values 1 to 9 concurrently in the given cell
+/**
+  * tryValuesConcurrently tries values 1 to 9 concurrently in the given cell
+  * @param puzzle The sudoku puzzle
+  * @param row The row of the cell
+  * @param col The column of the cell
+  * @return The solved sudoku puzzle
+**/
 func tryValuesConcurrently(puzzle [][]int, row int, col int) [][]int {
 	var wg sync.WaitGroup
 	solution := make(chan [][]int, 1)
@@ -56,7 +92,11 @@ func tryValuesConcurrently(puzzle [][]int, row int, col int) [][]int {
 	return nil
 }
 
-// makeCopyOfPuzzle creates a deep copy of the puzzle
+/**
+  * makeCopyOfPuzzle creates a deep copy of the puzzle
+  * @param src The sudoku puzzle to copy
+  * @return The copy of the sudoku puzzle
+**/
 func makeCopyOfPuzzle(src [][]int) [][]int {
 	dst := make([][]int, len(src))
 	for i := range src {
@@ -105,6 +145,28 @@ func isPossible(row int, col int, val int, puzzle [][]int) bool {
 }
 
 /**
+ * Checks if a sudoku puzzle is valid
+ * @param puzzle The sudoku puzzle to check
+ * @return True if the puzzle is valid, false otherwise
+ **/
+func isValid(puzzle [][]int) bool {
+	if len(puzzle) != 9 {
+		return false
+	}
+	if len(puzzle[0]) != 9 {
+		return false
+	}
+	for _, row := range puzzle {
+		for _, val := range row {
+			if val < 0 || val > 9 {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+/**
  * Prints a sudoku puzzle
  * @param puzzle The sudoku puzzle to print
  **/
@@ -120,29 +182,6 @@ func print(puzzle [][]int) {
 // The below code can be uncommented to print the solution. Replace the input with the puzzle you want to solve.
 /**
 func main() {
-	input := [][]int {
-		{5, 3, 0, 0, 7, 0, 0, 0, 0},
-		{6, 0, 0, 1, 9, 5, 0, 0, 0},
-		{0, 9, 8, 0, 0, 0, 0, 6, 0},
-		{8, 0, 0, 0, 6, 0, 0, 0, 3},
-		{4, 0, 0, 8, 0, 3, 0, 0, 1},
-		{7, 0, 0, 0, 2, 0, 0, 0, 6},
-		{0, 6, 0, 0, 0, 0, 2, 8, 0},
-		{0, 0, 0, 4, 1, 9, 0, 0, 5},
-		{0, 0, 0, 0, 8, 0, 0, 7, 9},
-	}
-
-	print(SolveSudoku(input))
-	// Uncomment the below line to measure the average run time
-	// measureAvgRunTime()
-
-}
-*/
-
-/**
- * Measures the average run time of the SolveSudoku function by running it 500 times
- **/
-func measureAvgRunTime() {
 	input := [][]int{
 		{5, 3, 0, 0, 7, 0, 0, 0, 0},
 		{6, 0, 0, 1, 9, 5, 0, 0, 0},
@@ -155,6 +194,19 @@ func measureAvgRunTime() {
 		{0, 0, 0, 0, 8, 0, 0, 7, 9},
 	}
 
+	if isValid(input) {
+		print(SolveSudoku(input))
+		// Uncomment the below line to measure the average run time
+		measureAvgRunTime(input)
+	}
+
+}
+*/
+
+/**
+ * Measures the average run time of the SolveSudoku function by running it 500 times
+ **/
+func measureAvgRunTime(input [][]int) {
 	startTime := time.Now()
 	for i := 0; i < 500; i++ {
 		SolveSudoku(input)
