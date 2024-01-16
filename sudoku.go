d35b312221aacbd4af1ba122a776fe9e743dda43
Zhenxu Wang <mason123wp@gmail.com>
2024-01-16 13:59:24 -0500

    add comments for SolveSudoku()

--- sudoku.go
+++ sudoku.go
@@ -1,5 +1,8 @@
 package main
 
+// SolveSudoku solves a provide sudoku grid
+// Input grid([][] int) of size 9 * 9 is guaranteed having only one solution
+// Returns solution in a grid([][] int) of size 9 * 9
 func SolveSudoku(input [][]int) [][]int {
 	return nil
 }
