9e3773a233a823a62c4176d959f7c5ce4b5b624e
justinjao <jao.c.justin@gmail.com>
2024-01-18 00:39:45 +0800

    add comments and cleanup code for readability

--- sudoku.go
+++ sudoku.go
@@ -2,25 +2,32 @@ package main
 
 import "math"
 
+// Scans board to determine if assigning currNum to position board[row][col] will conflict with an existing assignment.
 func isSafeToAssignCurrNum(board [][]int, row int, col int, currNum int) bool {
 
-	for j := 0; j < len(board); j++ {
+	var boardLength = len(board)
+
+	// Scans row to see if currNum already exists
+	for j := 0; j < boardLength; j++ {
 		if board[row][j] == currNum {
 			return false
 		}
 	}
-	for i := 0; i < len(board); i++ {
+
+	// Scans column to see if currNum already exists
+	for i := 0; i < boardLength; i++ {
 
 		if board[i][col] == currNum {
 			return false
 		}
 	}
 
-	sqrt := int(math.Sqrt(float64(len(board))))
-
+	// modulo is used to determine the box boundaries at current row/column position
+	sqrt := int(math.Sqrt(float64(boardLength)))
 	boxRowStart := row - row%sqrt
 	boxColStart := col - col%sqrt
 
+	// scans current box boundary to determine if currNum already exists
 	for i := boxRowStart; i < boxRowStart+sqrt; i++ {
 		for j := boxColStart; j < boxColStart+sqrt; j++ {
 			if board[i][j] == currNum {
@@ -29,34 +36,42 @@ func isSafeToAssignCurrNum(board [][]int, row int, col int, currNum int) bool {
 		}
 	}
 
+	// if scans never failed, then it is safe to assign currNum
 	return true
 }
 
+// A recursive backtracking function used to solve a sudoku board, by assigning a number to an empty square,
+// and recursively calling this function to assign a number to a different square. If at any point this fails, a square is
+// reset to 0 and is then assigned a different currNum. This continues until all square have been assigned.
 func solveBoard(board [][]int, n int) bool {
 	row := -1
 	col := -1
-	isBoardFilled := true
+	isFullyFilled := true
 
+	// find an empty square to be filled and assign indices to 'row' and 'col'
 	for i := 0; i < n; i++ {
 		for j := 0; j < n; j++ {
 			if board[i][j] == 0 {
 				row = i
 				col = j
 
-				isBoardFilled = false
+				isFullyFilled = false
 				break
 			}
 
 		}
-		if !isBoardFilled {
+		// if not fully filled, break out of for loop and continue to assigning numbers step
+		if !isFullyFilled {
 			break
 		}
 	}
 
-	if isBoardFilled {
+	// if fully filled, return as a correct solution has been found
+	if isFullyFilled {
 		return true
 	}
 
+	// from 1 to 9, try assigning a number to the empty square, backtracking if necessary
 	for num := 1; num <= n; num++ {
 		if isSafeToAssignCurrNum(board, row, col, num) {
 			board[row][col] = num
@@ -71,6 +86,7 @@ func solveBoard(board [][]int, n int) bool {
 	return false
 }
 
+// SolveSudoku is the master function that calls solveBoard to solve the sudoku puzzle, populating 'board' with the correct values.
 func SolveSudoku(board [][]int) [][]int {
 	solveBoard(board, len(board))
 	return board
