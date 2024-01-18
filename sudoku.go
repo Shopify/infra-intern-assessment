1f76aa482c000312574f4b49b240fd2994306459
Saad Khalid <1122brs221@gmail.com>
2024-01-17 23:51:11 -0500

    This is the completed algorithm, This is my first time writing in GO, I initally wrote the solution in python and adapted to GO syntax along the way. Added comments for clarity + to explain thought process

--- sudoku.go
+++ sudoku.go
@@ -1,54 +1,95 @@
 package main
 
-import "fmt"
-
-
-func main() {
-
-
-
-input := [][]int{
-	{5, 3, 0, 0, 7, 0, 0, 0, 0},
-	{6, 0, 0, 1, 9, 5, 0, 0, 0},
-	{0, 9, 8, 0, 0, 0, 0, 6, 0},
-	{8, 0, 0, 0, 6, 0, 0, 0, 3},
-	{4, 0, 0, 8, 0, 3, 0, 0, 1},
-	{7, 0, 0, 0, 2, 0, 0, 0, 6},
-	{0, 6, 0, 0, 0, 0, 2, 8, 0},
-	{0, 0, 0, 4, 1, 9, 0, 0, 5},
-	{0, 0, 0, 0, 8, 0, 0, 7, 9},
-}
-
-
-	solved := SolveSudoku(input)
-
-
-	
-	// expected := [][]int{
-	// 	{5, 3, 4, 6, 7, 8, 9, 1, 2},
-	// 	{6, 7, 2, 1, 9, 5, 3, 4, 8},
-	// 	{1, 9, 8, 3, 4, 2, 5, 6, 7},
-	// 	{8, 5, 9, 7, 6, 1, 4, 2, 3},
-	// 	{4, 2, 6, 8, 5, 3, 7, 9, 1},
-	// 	{7, 1, 3, 9, 2, 4, 8, 5, 6},
-	// 	{9, 6, 1, 5, 3, 7, 2, 8, 4},
-	// 	{2, 8, 7, 4, 1, 9, 6, 3, 5},
-	// 	{3, 4, 5, 2, 8, 6, 1, 7, 9},
-	// }
-	
-	// print results
-	for row := 0; row < 9; row++ {
-		for col := 0; col < 9; col++ {
-			fmt.Printf("%d ", solved[row][col])
-		}
-		fmt.Printf("\n")
-	}
-
-}
-
-
 // this is just the setup, will construct and debug from here
-func SolveSudoku(p [][]int) [][]int {
-	return p
+func SolveSudoku(puzzle [][]int) [][]int {
+	solve(puzzle)
+	return puzzle
 }
+
+func solve(SudokuBoard [][]int) bool {
+
+	const BOARD_SIZE = 9
+	const EMPTY_CELL = 0
+
+	// we use these loops to iterate through each 
+	// grid square [i][j] 
+	for i := 0; i < BOARD_SIZE; i++ {
+			for j := 0; j < BOARD_SIZE; j++ {
+
+					// Preliminary check to see if square is empty
+					if SudokuBoard[i][j] == EMPTY_CELL {
+
+							// we try all values of n until 
+							// one of them is valid 
+							for n := 1; n <= 9; n++ {
+
+									// we place the first valid value of n into (i, j)
+									if canPlaceN(SudokuBoard, i, j, n) {
+										SudokuBoard[i][j] = n
+
+
+											// this is a recursive call to
+											// puzzle, essentially, what is happening here is
+											// solve(puzzle) branches into two cases 
+											// (1) inserting n into (i, j) is valid in a solution
+											//  	 of the puzzle, in which case solve(puzzle)
+											//  	 continues where we left off, and returns true
+											//     once completing the puzzle 
+											// (2) inserting n into (i, j) is NOT a valid step 
+											// 	   in a solution of the puzzle (given all previous
+											//		 steps are set in stone), in which case we backtrack 
+											// 	   and try the next value of n.	
+											if solve(SudokuBoard) {
+													return true
+											}
+
+											SudokuBoard[i][j] = EMPTY_CELL // backtrack
+									}
+							}
+							return false
+					}
+			}
+	}
+	return true
+}
+
+
+
+
+func canPlaceN(p [][]int, i, j, n int) bool {
+	const BOARD_SIZE = 9
+	const SUBGRID_SIZE = 3
+
+	// this function checks if we can place a number (n) at position (i, j)
+	// in the sudoku grid (9x9 size assumed)
+	for k := 0; k < BOARD_SIZE; k++ {
+			// if we find another instance of 
+			// n in the same col/row, we return early 
+			if p[i][k] == n || p[k][j] == n {
+					return false
+			}
+
+			// this part essentially 
+			// patitions the 9x9 grid into 
+			// 9 3x3 grids, and treats each i
+			// as its own subspace, checking if
+			// n already exists in the grid. 
+			subGridRow := SUBGRID_SIZE * (i / SUBGRID_SIZE) 
+			subGridColumn := SUBGRID_SIZE * (j / SUBGRID_SIZE)
+
+
+			rowOffset, colOffset := divmod(k, SUBGRID_SIZE)
+
+			// we use the index of the grid 
+			if p[subGridRow + rowOffset][subGridColumn + colOffset] == n {
+					return false
+			}
+	}
+	return true 
+}
+
+func divmod(numerator, denominator int) (int, int) {
+	return numerator / denominator, numerator % denominator 
+}
+
 
