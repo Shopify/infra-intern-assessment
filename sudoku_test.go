12c5bd21bed147f89928922fa60cd2098fab7082
mihirKachroo <mkachroo29@gmail.com>
2024-01-17 22:11:58 -0500

    feature: table-driven testing

--- sudoku_test.go
+++ sudoku_test.go
@@ -5,34 +5,48 @@ import (
 	"testing"
 )
 
+// Table-driven tests implementation
 func TestSolveSudoku(t *testing.T) {
-	input := [][]int{
-		{5, 3, 0, 0, 7, 0, 0, 0, 0},
-		{6, 0, 0, 1, 9, 5, 0, 0, 0},
-		{0, 9, 8, 0, 0, 0, 0, 6, 0},
-		{8, 0, 0, 0, 6, 0, 0, 0, 3},
-		{4, 0, 0, 8, 0, 3, 0, 0, 1},
-		{7, 0, 0, 0, 2, 0, 0, 0, 6},
-		{0, 6, 0, 0, 0, 0, 2, 8, 0},
-		{0, 0, 0, 4, 1, 9, 0, 0, 5},
-		{0, 0, 0, 0, 8, 0, 0, 7, 9},
+	// Define struct for test cases
+	type testCase struct {
+		input    [][]int
+		expected [][]int
 	}
 
-	expected := [][]int{
-		{5, 3, 4, 6, 7, 8, 9, 1, 2},
-		{6, 7, 2, 1, 9, 5, 3, 4, 8},
-		{1, 9, 8, 3, 4, 2, 5, 6, 7},
-		{8, 5, 9, 7, 6, 1, 4, 2, 3},
-		{4, 2, 6, 8, 5, 3, 7, 9, 1},
-		{7, 1, 3, 9, 2, 4, 8, 5, 6},
-		{9, 6, 1, 5, 3, 7, 2, 8, 4},
-		{2, 8, 7, 4, 1, 9, 6, 3, 5},
-		{3, 4, 5, 2, 8, 6, 1, 7, 9},
+	// Create a slice of test cases
+	tests := []testCase{
+		{
+			input : [][]int{
+				{5, 3, 0, 0, 7, 0, 0, 0, 0},
+				{6, 0, 0, 1, 9, 5, 0, 0, 0},
+				{0, 9, 8, 0, 0, 0, 0, 6, 0},
+				{8, 0, 0, 0, 6, 0, 0, 0, 3},
+				{4, 0, 0, 8, 0, 3, 0, 0, 1},
+				{7, 0, 0, 0, 2, 0, 0, 0, 6},
+				{0, 6, 0, 0, 0, 0, 2, 8, 0},
+				{0, 0, 0, 4, 1, 9, 0, 0, 5},
+				{0, 0, 0, 0, 8, 0, 0, 7, 9},
+			},
+			expected : [][]int{
+				{5, 3, 4, 6, 7, 8, 9, 1, 2},
+				{6, 7, 2, 1, 9, 5, 3, 4, 8},
+				{1, 9, 8, 3, 4, 2, 5, 6, 7},
+				{8, 5, 9, 7, 6, 1, 4, 2, 3},
+				{4, 2, 6, 8, 5, 3, 7, 9, 1},
+				{7, 1, 3, 9, 2, 4, 8, 5, 6},
+				{9, 6, 1, 5, 3, 7, 2, 8, 4},
+				{2, 8, 7, 4, 1, 9, 6, 3, 5},
+				{3, 4, 5, 2, 8, 6, 1, 7, 9},
+			},
+		},
 	}
 
-	solved := SolveSudoku(input)
-
-	if !reflect.DeepEqual(solved, expected) {
-		t.Errorf("Sudoku puzzle was not solved correctly. Expected:\n%v\n\nGot:\n%v", expected, solved)
+	// Iterate over test cases individually
+	for _, test := range tests {
+		solved := SolveSudoku(test.input)
+		if !reflect.DeepEqual(solved, test.expected) {
+			t.Errorf("Sudoku puzzle was not solved correctly. Expected:\n%v\n\nGot:\n%v", test.expected, solved)
+		}
 	}
+	
 }
