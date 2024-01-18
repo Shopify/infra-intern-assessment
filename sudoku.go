bc3017f9487956a7771ebf318e66472a477247c1
Zhenxu Wang <mason123wp@gmail.com>
2024-01-17 19:35:11 -0500

    finalize comments

--- sudoku.go
+++ sudoku.go
@@ -12,12 +12,12 @@ func SolveSudoku(input [][]int) [][]int {
 func backtracking(grid *[][]int, row int, col int) bool {
 	rowMax, colMax := len(*grid), len((*grid)[0])
 
-	// case#2: base case,
+	// Base case: if we have proceeded all rows, sudoku solved. Return true
 	if row == rowMax {
 		return true
 	}
 
-	// case#1: where the end of a column is reached, go the start of next row
+	// If the end of a column is reached, go the start of next row
 	if col == colMax {
 		return backtracking(grid, row+1, 0)
 	}
@@ -27,21 +27,26 @@ func backtracking(grid *[][]int, row int, col int) bool {
 		return backtracking(grid, row, col+1)
 	}
 
+	// Try all values (1 to 9) for current cell
 	for value := 1; value <= 9; value++ {
-		// if value is not valid for current cell, try next value
+		// If value is not valid for current cell, try next value
 		if !validate(value, grid, row, col) {
 			continue
 		}
 
-		(*grid)[row][col] = value // assign validated value
+		// Assign validated value
+		(*grid)[row][col] = value
 
+		// If true, sudoku solution is found, return true
 		if backtracking(grid, row, col+1) {
 			return true
 		}
 
+		// Otherwise, this value is not correct proceed backtrack
 		(*grid)[row][col] = 0
 	}
 
+	// No solution is found in this call and its recursive calls, return false and backtrack
 	return false
 }
 
@@ -49,7 +54,7 @@ func backtracking(grid *[][]int, row int, col int) bool {
 // row, col are the coordinates of the value in the grid
 // Returns true if value is valid, false otherwise
 func validate(value int, grid *[][]int, row int, col int) bool {
-	// check if value is duplicated in current row and column
+	// Check if value is duplicated in current row and column
 	for i := 0; i < 9; i++ {
 		if (*grid)[i][col] == value || (*grid)[row][i] == value {
 			return false
@@ -64,10 +69,10 @@ func validate(value int, grid *[][]int, row int, col int) bool {
 		[ ][ ][ ]
 	*/
 	const blockConv int = 3
-	blockRow := row - row%blockConv // actual row index
-	blockCol := col - col%blockConv // actual column index
+	blockRow := row - row%blockConv // Actual row index
+	blockCol := col - col%blockConv // Actual column index
 
-	// check if value is duplicated in the 3*3 block
+	// Check if value is duplicated in the 3*3 block
 	for i := 0; i < 3; i++ {
 		for j := 0; j < 3; j++ {
 			if (*grid)[blockRow+i][blockCol+j] == value {
@@ -76,6 +81,6 @@ func validate(value int, grid *[][]int, row int, col int) bool {
 		}
 	}
 
-	// if value is not duplicated, this value is valid
+	// If value is not duplicated, this value is valid
 	return true
 }
