3c63ed4c978360f3b6699090b53e875bf645d940
Theo(dore) <theo@pta.gg>
2024-01-18 20:20:43 -0500

    cleanup pass

--- sudoku.go
+++ sudoku.go
@@ -1,33 +1,25 @@
 package main
 
-// Convert an index to a subgrid number
+// Convert an index to a sub-grid number
 func iToSg(i int) int {
-	if i < 27 {
-		return (i % 9) / 3
-	} else if i < 54 {
-		return 3 + (i%9)/3
-	} else {
-		return 6 + (i%9)/3
-	}
+	return (i/27)*3 + (i%9)/3
+	// Note: /27*3 is not the same as /9 (because integer division).
 }
 
+// DFS to find the solution.
 func solveSudokuHelper(i int, board [][]int, colUsed [9][9]bool, rowUsed [9][9]bool, sgUsed [9][9]bool) bool {
-	// If the last looked at index was the last index, we are done
+	// If the current index is off the end of the board... we're done!
 	if i == 81 {
 		return true
 	}
 
-	// Find the next free index to look at
-	for board[i/9][i%9] != 0 {
-		i++
-
-		if i == 81 {
-			return true
-		}
+	// If the current index is already set... skip to the next index.
+	if board[i/9][i%9] != 0 {
+		return solveSudokuHelper(i+1, board, colUsed, rowUsed, sgUsed)
 	}
 
-	// Go through all possible numbers not yet set
-	for target, _ := range board {
+	// Otherwise, go through all feasible numbers.
+	for target := range board {
 		if colUsed[i%9][target] || rowUsed[i/9][target] || sgUsed[iToSg(i)][target] {
 			continue
 		}
@@ -51,20 +43,28 @@ func solveSudokuHelper(i int, board [][]int, colUsed [9][9]bool, rowUsed [9][9]b
 
 func SolveSudoku(board [][]int) [][]int {
 	colUsed := [9][9]bool{} // colUsed[i][j]==true implies j+1 has not yet been set in col i
-	rowUsed := [9][9]bool{} // rowUsed[i][j]==true implies j+1 has not yet been set in row j
-	sgUsed := [9][9]bool{}  // sgUsed[i][j]==true implies j+1 has not yet been set in sub grid j
+	rowUsed := [9][9]bool{} // rowUsed[i][j]==true implies j+1 has not yet been set in row i
+	sgUsed := [9][9]bool{}  // sgUsed[i][j]==true implies j+1 has not yet been set in sub-grid i
 
-	for i, _ := range board {
-		for j, _ := range board {
-			target := board[i][j]
-			if target != 0 {
-				rowUsed[i][target-1] = true
-				colUsed[j][target-1] = true
-				sgUsed[iToSg(9*i+j)][target-1] = true
+	for y := range board {
+		for x := range board {
+			target := board[y][x]
+			if target == 0 {
+				continue
 			}
+
+			rowUsed[y][target-1] = true
+			colUsed[x][target-1] = true
+			sgUsed[iToSg(9*y+x)][target-1] = true
 		}
 	}
 
-	solveSudokuHelper(0, board, colUsed, rowUsed, sgUsed)
-	return board
+	solution := make([][]int, len(board))
+	for y := range solution {
+		solution[y] = make([]int, len(board[y]))
+		copy(solution[y], board[y])
+	}
+
+	solveSudokuHelper(0, solution, colUsed, rowUsed, sgUsed)
+	return solution
 }
