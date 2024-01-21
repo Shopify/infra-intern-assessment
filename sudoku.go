45aaadc0560559845190247e06e9c8687c749555
zhaojer <zhaojer@umich.edu>
2024-01-20 20:37:02 -0500

    Set up environment and begin implementation

--- sudoku.go
+++ sudoku.go
@@ -1,1 +1,32 @@
 package main
+
+import "fmt"
+
+// Requires: The input grid will be a 9x9 two-dimensional array of integers.
+//			 The input grid will have exactly one solution.
+// Modifies: N/A
+// Effects: Solves the sudoku, returns a 9 by 9 array of the solved sudoku
+func SolveSudoku(sudoku [][]int) [][]int {
+    for i := 0; i < len(sudoku); i++ {
+        for j := 0; j < len(sudoku[i]); j++ {
+            fmt.Printf("%d ", sudoku[i][j])
+        }
+        fmt.Println()
+    }
+	return sudoku
+}
+
+func main() {
+	input := [][]int{
+		{5, 3, 0, 0, 7, 0, 0, 0, 0},
+		{6, 0, 0, 1, 9, 5, 0, 0, 0},
+		{0, 9, 8, 0, 0, 0, 0, 6, 0},
+		{8, 0, 0, 0, 6, 0, 0, 0, 3},
+		{4, 0, 0, 8, 0, 3, 0, 0, 1},
+		{7, 0, 0, 0, 2, 0, 0, 0, 6},
+		{0, 6, 0, 0, 0, 0, 2, 8, 0},
+		{0, 0, 0, 4, 1, 9, 0, 0, 5},
+		{0, 0, 0, 0, 8, 0, 0, 7, 9},
+	}
+	SolveSudoku(input)
+}
