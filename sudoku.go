b0b41204eebbf410dc7a7a2f529c25ee5f1a21d3
zhaojer <zhaojer@umich.edu>
2024-01-21 17:56:43 -0500

    Clean up unnecessary code

--- sudoku.go
+++ sudoku.go
@@ -120,23 +120,13 @@ func SolveSudoku(sudoku [][]int) [][]int {
 	return sudoku
 }
 
-func main() {
-	input := [][]int{
-		{5, 3, 0, 0, 7, 0, 0, 0, 0},
-		{6, 0, 0, 1, 9, 5, 0, 0, 0},
-		{0, 9, 8, 0, 0, 0, 0, 6, 0},
-		{8, 0, 0, 0, 6, 0, 0, 0, 3},
-		{4, 0, 0, 8, 0, 3, 0, 0, 1},
-		{7, 0, 0, 0, 2, 0, 0, 0, 6},
-		{0, 6, 0, 0, 0, 0, 2, 8, 0},
-		{0, 0, 0, 4, 1, 9, 0, 0, 5},
-		{0, 0, 0, 0, 8, 0, 0, 7, 9},
-	}
-	SolveSudoku(input)
-
-    for i := 0; i < len(input); i++ {
-        for j := 0; j < len(input[i]); j++ {
-            fmt.Printf("%d ", input[i][j])
+// REQUIRES: The input grid will be a 9x9 two-dimensional array of integers.
+// MODIFIES: N/A
+// EFFECTS: Prints the sudoku board
+func PrintSudoku(sudoku [][]int) {
+	for i := 0; i < len(sudoku); i++ {
+        for j := 0; j < len(sudoku[i]); j++ {
+            fmt.Printf("%d ", sudoku[i][j])
         }
         fmt.Println()
     }
