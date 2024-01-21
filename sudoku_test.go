77f68c3c014b601710b469a26c2baf40fa177d11
zhaojer <zhaojer@umich.edu>
2024-01-21 15:30:01 -0500

    Write some edge case tests

--- sudoku_test.go
+++ sudoku_test.go
@@ -5,6 +5,131 @@ import (
 	"testing"
 )
 
+// Test: sudoku is already solved
+func TestAllFilled(t *testing.T) {
+	input := [][]int{
+		{5, 3, 4, 6, 7, 8, 9, 1, 2},
+		{6, 7, 2, 1, 9, 5, 3, 4, 8},
+		{1, 9, 8, 3, 4, 2, 5, 6, 7},
+		{8, 5, 9, 7, 6, 1, 4, 2, 3},
+		{4, 2, 6, 8, 5, 3, 7, 9, 1},
+		{7, 1, 3, 9, 2, 4, 8, 5, 6},
+		{9, 6, 1, 5, 3, 7, 2, 8, 4},
+		{2, 8, 7, 4, 1, 9, 6, 3, 5},
+		{3, 4, 5, 2, 8, 6, 1, 7, 9},
+	}
+	expected := [][]int{
+		{5, 3, 4, 6, 7, 8, 9, 1, 2},
+		{6, 7, 2, 1, 9, 5, 3, 4, 8},
+		{1, 9, 8, 3, 4, 2, 5, 6, 7},
+		{8, 5, 9, 7, 6, 1, 4, 2, 3},
+		{4, 2, 6, 8, 5, 3, 7, 9, 1},
+		{7, 1, 3, 9, 2, 4, 8, 5, 6},
+		{9, 6, 1, 5, 3, 7, 2, 8, 4},
+		{2, 8, 7, 4, 1, 9, 6, 3, 5},
+		{3, 4, 5, 2, 8, 6, 1, 7, 9},
+	}
+	solved := SolveSudoku(input)
+	if !reflect.DeepEqual(solved, expected) {
+		t.Errorf("Sudoku puzzle was not solved correctly. Expected:\n%v\n\nGot:\n%v", expected, solved)
+	}
+}
+
+
+// Test: exactly 1 cell is 0
+func TestOneEmptyCell(t *testing.T) {
+	input := [][]int{
+		{5, 3, 4, 6, 7, 8, 9, 1, 2},
+		{6, 7, 2, 1, 9, 5, 3, 4, 8},
+		{1, 9, 8, 3, 4, 2, 5, 6, 7},
+		{8, 5, 9, 7, 6, 1, 4, 2, 3},
+		{4, 2, 6, 8, 0, 3, 7, 9, 1}, // an empty cell here
+		{7, 1, 3, 9, 2, 4, 8, 5, 6},
+		{9, 6, 1, 5, 3, 7, 2, 8, 4},
+		{2, 8, 7, 4, 1, 9, 6, 3, 5},
+		{3, 4, 5, 2, 8, 6, 1, 7, 9},
+	}
+	expected := [][]int{
+		{5, 3, 4, 6, 7, 8, 9, 1, 2},
+		{6, 7, 2, 1, 9, 5, 3, 4, 8},
+		{1, 9, 8, 3, 4, 2, 5, 6, 7},
+		{8, 5, 9, 7, 6, 1, 4, 2, 3},
+		{4, 2, 6, 8, 5, 3, 7, 9, 1},
+		{7, 1, 3, 9, 2, 4, 8, 5, 6},
+		{9, 6, 1, 5, 3, 7, 2, 8, 4},
+		{2, 8, 7, 4, 1, 9, 6, 3, 5},
+		{3, 4, 5, 2, 8, 6, 1, 7, 9},
+	}
+	solved := SolveSudoku(input)
+	if !reflect.DeepEqual(solved, expected) {
+		t.Errorf("Sudoku puzzle was not solved correctly. Expected:\n%v\n\nGot:\n%v", expected, solved)
+	}
+}
+
+// Test: exactly 1 cell in each row, col, grid is 0
+func TestNineEmptyCells(t *testing.T) {
+	input := [][]int{
+		{0, 3, 4, 6, 7, 8, 9, 1, 2},
+		{6, 7, 2, 1, 0, 5, 3, 4, 8},
+		{1, 9, 8, 3, 4, 2, 5, 6, 0},
+		{8, 5, 9, 0, 6, 1, 4, 2, 3},
+		{4, 2, 6, 8, 5, 3, 7, 0, 1},
+		{7, 0, 3, 9, 2, 4, 8, 5, 6},
+		{9, 6, 1, 5, 3, 7, 0, 8, 4},
+		{2, 8, 0, 4, 1, 9, 6, 3, 5},
+		{3, 4, 5, 2, 8, 0, 1, 7, 9},
+	}
+	expected := [][]int{
+		{5, 3, 4, 6, 7, 8, 9, 1, 2},
+		{6, 7, 2, 1, 9, 5, 3, 4, 8},
+		{1, 9, 8, 3, 4, 2, 5, 6, 7},
+		{8, 5, 9, 7, 6, 1, 4, 2, 3},
+		{4, 2, 6, 8, 5, 3, 7, 9, 1},
+		{7, 1, 3, 9, 2, 4, 8, 5, 6},
+		{9, 6, 1, 5, 3, 7, 2, 8, 4},
+		{2, 8, 7, 4, 1, 9, 6, 3, 5},
+		{3, 4, 5, 2, 8, 6, 1, 7, 9},
+	}
+	solved := SolveSudoku(input)
+	if !reflect.DeepEqual(solved, expected) {
+		t.Errorf("Sudoku puzzle was not solved correctly. Expected:\n%v\n\nGot:\n%v", expected, solved)
+	}
+}
+
+// Test: unfilled cells are not represented by 0
+func TestWeirdNumbers(t *testing.T) {
+	input := [][]int{
+		{5, 3, -1, 0, 7, 0, 0, 0, 10},
+		{6, 0, 0, 1, 9, 5, 0, 10, 50},
+		{0, 9, 8, -90, 0, 0, 0, 6, 0},
+		{8, 0, 500, 0, 6, 0, 0, 0, 3},
+		{4, 20, 0, 8, 0, 3, 0, 10, 1},
+		{7, 0, -1, 0, 2, 0, 79, 0, 6},
+		{0, 6, 0, 19, 0, 0, 2, 8, -1},
+		{45, 0, 0, 4, 1, 9, 0, -1, 5},
+		{-10, 0, 0, 0, 8, 0, 0, 7, 9},
+	}
+
+	expected := [][]int{
+		{5, 3, 4, 6, 7, 8, 9, 1, 2},
+		{6, 7, 2, 1, 9, 5, 3, 4, 8},
+		{1, 9, 8, 3, 4, 2, 5, 6, 7},
+		{8, 5, 9, 7, 6, 1, 4, 2, 3},
+		{4, 2, 6, 8, 5, 3, 7, 9, 1},
+		{7, 1, 3, 9, 2, 4, 8, 5, 6},
+		{9, 6, 1, 5, 3, 7, 2, 8, 4},
+		{2, 8, 7, 4, 1, 9, 6, 3, 5},
+		{3, 4, 5, 2, 8, 6, 1, 7, 9},
+	}
+
+	solved := SolveSudoku(input)
+
+	if !reflect.DeepEqual(solved, expected) {
+		t.Errorf("Sudoku puzzle was not solved correctly. Expected:\n%v\n\nGot:\n%v", expected, solved)
+	}
+}
+
+// Given test case
 func TestSolveSudoku(t *testing.T) {
 	input := [][]int{
 		{5, 3, 0, 0, 7, 0, 0, 0, 0},
