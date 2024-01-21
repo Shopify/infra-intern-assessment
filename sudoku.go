dea9005b758ede67ebdeb06877598fa53ddde8f0
zhaojer <zhaojer@umich.edu>
2024-01-21 14:45:19 -0500

    Implement constraint checking

--- sudoku.go
+++ sudoku.go
@@ -2,6 +2,32 @@ package main
 
 import "fmt"
 
+// REQUIRES: existing board is valid
+// 			 0 <= row, col <= 8; board[row][col] is unfilled
+//			 1 <= num <= 9
+// MODIFIES: N/A
+// EFFECTS: Determines if placing num at board[row][col] is valid
+func Promising(board [][]int, row int, col int, num int) bool {
+	// check all cells on the same row and same column as the cell at [row][col]
+	for i := 0; i < 9; i++ {
+		if board[row][i] == num || board[i][col] == num {
+			return false
+		}
+	}
+	// check all cells in the same 3 by 3 grid as the cell at [row][col]
+	startRowIdx := (row / 3) * 3
+	startColIdx := (col / 3) * 3
+	fmt.Printf("Start row idx: %d, Start col idx: %d\n", startRowIdx, startColIdx)
+	for i := startRowIdx; i < startRowIdx + 3; i++ {
+		for j := startColIdx; j < startColIdx + 3; j++ {
+			if board[i][j] == num {
+				return false
+			}
+		}
+	}
+	return true
+}
+
 // REQUIRES: board is a 2d array representing a sudoku with exactly 1 solution
 //			 0 <= row, col <= 8
 // MODIFIES: board
@@ -10,10 +36,17 @@ func SolveSudokuHelper(board [][]int, row int, col int) bool {
 	// If sudoku is solved, return true
 	// Otherwise
 	// Try to place number 1 through 9 at current row and col
+	for num := 1; num <= 9; num++ {
 		// Check constraint: If placing this number does not violate any rules of sudoku
+		if Promising(board, row, col, num) {
+			// Place number
+			board[row][col] = num
 			// Recursion with the next unfilled cell on the board
+
 			// If recursive function call returned true, then it means puzzle solved so return true
 		// Otherwise, Backtrack: "unplace" the number at current cell
+		}
+	}
 	// Reaching this point means that none of the numbers from 1 to 9 can be placed at current cell
 	// i.e. the number in a previous cell needs to change, thus Backtrack -> return to the caller!
 	return false
@@ -36,7 +69,7 @@ func SolveSudoku(sudoku [][]int) [][]int {
 	return sudoku
 }
 
-func main() {
+func testPromising() {
 	input := [][]int{
 		{5, 3, 0, 0, 7, 0, 0, 0, 0},
 		{6, 0, 0, 1, 9, 5, 0, 0, 0},
@@ -48,5 +81,39 @@ func main() {
 		{0, 0, 0, 4, 1, 9, 0, 0, 5},
 		{0, 0, 0, 0, 8, 0, 0, 7, 9},
 	}
-	SolveSudoku(input)
+	fmt.Printf("Expected: %t; Got: %t\n", false, Promising(input, 2, 0, 5))
+	fmt.Printf("Expected: %t; Got: %t\n", false, Promising(input, 2, 0, 7))
+	fmt.Printf("Expected: %t; Got: %t\n", false, Promising(input, 2, 0, 4))
+	fmt.Printf("Expected: %t; Got: %t\n", false, Promising(input, 2, 0, 9))
+	fmt.Printf("Expected: %t; Got: %t\n", false, Promising(input, 2, 0, 3))
+	fmt.Printf("Expected: %t; Got: %t\n", false, Promising(input, 2, 0, 8))
+	fmt.Printf("Expected: %t; Got: %t\n", false, Promising(input, 2, 0, 6))
+	fmt.Printf("Expected: %t; Got: %t\n", true, Promising(input, 2, 0, 1))
+	fmt.Printf("Expected: %t; Got: %t\n", true, Promising(input, 2, 0, 2))
+
+	fmt.Printf("Expected: %t; Got: %t\n", false, Promising(input, 7, 6, 7))
+	fmt.Printf("Expected: %t; Got: %t\n", false, Promising(input, 7, 6, 9))
+	fmt.Printf("Expected: %t; Got: %t\n", false, Promising(input, 7, 6, 8))
+
+	fmt.Printf("Expected: %t; Got: %t\n", false, Promising(input, 8, 5, 4))
+	fmt.Printf("Expected: %t; Got: %t\n", false, Promising(input, 8, 5, 1))
+	fmt.Printf("Expected: %t; Got: %t\n", true, Promising(input, 8, 5, 6))
+	fmt.Printf("Expected: %t; Got: %t\n", true, Promising(input, 8, 5, 2))
+}
+
+func main() {
+	// input := [][]int{
+	// 	{5, 3, 0, 0, 7, 0, 0, 0, 0},
+	// 	{6, 0, 0, 1, 9, 5, 0, 0, 0},
+	// 	{0, 9, 8, 0, 0, 0, 0, 6, 0},
+	// 	{8, 0, 0, 0, 6, 0, 0, 0, 3},
+	// 	{4, 0, 0, 8, 0, 3, 0, 0, 1},
+	// 	{7, 0, 0, 0, 2, 0, 0, 0, 6},
+	// 	{0, 6, 0, 0, 0, 0, 2, 8, 0},
+	// 	{0, 0, 0, 4, 1, 9, 0, 0, 5},
+	// 	{0, 0, 0, 0, 8, 0, 0, 7, 9},
+	// }
+	// SolveSudoku(input)
+
+	testPromising()
 }
