5cefd7fe914b1f673d90fd43f6a115bdb8b3310f
sam-ayo <sam-ayo>
2024-01-23 18:33:36 -0330

    commit all changes

--- sudoku.go
+++ sudoku.go
@@ -28,24 +28,29 @@ func isPossible(grid [][]int, row int, col int, n int) bool {
 	return true
 }
 
-func isSolved(grid [][]int, row int, col int) bool {
-	// Move along the board
-	if row == BOARD_SIZE {
-		row = 0
-		if col++; col == BOARD_SIZE {
-			return true
+func findNextEmptyCell(grid [][]int) (int, int, bool) {
+	for i:=0; i<BOARD_SIZE; i++ {
+		for j:=0; j<BOARD_SIZE; j++ {
+			if grid[i][j] == 0{
+				return i, j, true
+			}
 		}
 	}
+	return -1, -1, false
+}
 
-	if grid[row][col] != 0 {
-		return isSolved(grid, row+1, col)
+
+func isSolved(grid [][]int) bool {
+	row, col, found := findNextEmptyCell(grid)
+	if !found {
+		return true
 	}
-
+	
 	// try numbers from 1 - BOARD_SIZE and back track if bad choices have been made 
 	for n := 1; n <= BOARD_SIZE; n++ {
 		if isPossible(grid, row, col, n) {
 			grid[row][col] = n
-			if isSolved(grid, row+1, col) {
+			if isSolved(grid) {
 				return true
 			}
 			grid[row][col] = 0
@@ -57,6 +62,6 @@ func isSolved(grid [][]int, row int, col int) bool {
 
 func SolveSudoku(grid[][]int) [][]int {
 	// Assuming that we always get a valid sudoku board
-	isSolved(grid, 0, 0) 
+	isSolved(grid)
 	return grid
 }
