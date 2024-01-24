bee42489c05d8850f0f8440ab31504e5d00a1d82
cnguye18 <66088422+Cody-Newguyin@users.noreply.github.com>
2024-01-24 16:55:09 -0500

    add copying and printing grid

--- sudoku.go
+++ sudoku.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"fmt"
+)
+
+func SolveSudoku(grid [][]int) [][]int {
+	solvedGrid := CopyGrid(grid)
+	return SolveGridNaive(solvedGrid, 0, 0)
+}
+
+func PrintSudoku(grid [][]int) {
+	for i := range grid {
+		fmt.Printf("%v\n", grid[i])
+	}
+}
+
+func CopyGrid(grid [][]int) [][]int {
+	tempGrid := make([][]int, len(grid))
+	for i := range grid {
+		tempGrid[i] = make([]int, len(grid[i]))
+		copy(tempGrid[i], grid[i])
+	}
+	return tempGrid
+}
+
+func SolveGridNaive(grid [][]int, row int, col int) [][]int {
+	for i := range grid {
+		for j := range grid[i] {
+			grid[i][j] = 0
+		}
+	}
+	return grid
+}
