950e1881de6966a49742664cdaa7c17f52ac5f65
amin <shafiee.mohamin@gmail.com>
2024-01-25 13:16:36 -0500

    solveSoduku function added to pass the test

--- sudoku.go
+++ sudoku.go
@@ -1,1 +1,10 @@
 package main
+
+func SolveSudoku(grid [][]int) [][]int {
+	sodoku := NewSodoku(grid, GRID_ROW_SIZE, GRID_COL_SIZE)
+	sodoku.Solve()
+	if sodoku.solved {
+		return sodoku.solvedGrid
+	}
+	return sodoku.grid
+}
