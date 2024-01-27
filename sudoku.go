0faa89be93ebbba0a6979fa3781b72650691207c
mgdzura <mgdzura@myseneca.ca>
2024-01-27 12:35:33 -0500

    Second attempt - passed

--- sudoku.go
+++ sudoku.go
@@ -4,34 +4,35 @@ import "fmt"
 
 const N = 9
 
-// SolveSudoku solves the Sudoku puzzle
-func SolveSudoku(grid [N][N]int) bool {
+// SolveSudoku solves the Sudoku puzzle. It returns the solved grid.
+func SolveSudoku(grid [][]int) [][]int {
+	if solveSudokuHelper(grid) {
+		return grid // Return the solved grid
+	}
+	return grid // Return the original grid if unsolvable
+}
+
+// solveSudokuHelper is a helper function that applies the backtracking algorithm.
+func solveSudokuHelper(grid [][]int) bool {
 	var row, col int
 	if !findEmptyLocation(grid, &row, &col) {
-		// No empty location found, puzzle solved
-		return true
+		return true // Puzzle solved
 	}
 
 	for num := 1; num <= 9; num++ {
-		// Check if number can be placed
 		if isSafe(grid, row, col, num) {
-			// Make tentative assignment
 			grid[row][col] = num
-
-			// Return if success
-			if SolveSudoku(grid) {
+			if solveSudokuHelper(grid) {
 				return true
 			}
-
-			// Failure, unmake & try again
-			grid[row][col] = 0
+			grid[row][col] = 0 // Failure, unmake & try again
 		}
 	}
 	return false // Triggers backtracking
 }
 
 // findEmptyLocation searches for a location that is still unassigned
-func findEmptyLocation(grid [N][N]int, row, col *int) bool {
+func findEmptyLocation(grid [][]int, row, col *int) bool {
 	for *row = 0; *row < N; *row++ {
 		for *col = 0; *col < N; *col++ {
 			if grid[*row][*col] == 0 {
@@ -43,12 +44,12 @@ func findEmptyLocation(grid [N][N]int, row, col *int) bool {
 }
 
 // isSafe checks if it will be legal to assign num to the given row, col
-func isSafe(grid [N][N]int, row, col, num int) bool {
+func isSafe(grid [][]int, row, col, num int) bool {
 	return !usedInRow(grid, row, num) && !usedInCol(grid, col, num) && !usedInBox(grid, row-row%3, col-col%3, num)
 }
 
 // usedInRow checks if a number is in the given row
-func usedInRow(grid [N][N]int, row, num int) bool {
+func usedInRow(grid [][]int, row, num int) bool {
 	for col := 0; col < N; col++ {
 		if grid[row][col] == num {
 			return true
@@ -58,7 +59,7 @@ func usedInRow(grid [N][N]int, row, num int) bool {
 }
 
 // usedInCol checks if a number is in the given column
-func usedInCol(grid [N][N]int, col, num int) bool {
+func usedInCol(grid [][]int, col, num int) bool {
 	for row := 0; row < N; row++ {
 		if grid[row][col] == num {
 			return true
@@ -68,7 +69,7 @@ func usedInCol(grid [N][N]int, col, num int) bool {
 }
 
 // usedInBox checks if a number is in the given 3x3 box
-func usedInBox(grid [N][N]int, boxStartRow, boxStartCol, num int) bool {
+func usedInBox(grid [][]int, boxStartRow, boxStartCol, num int) bool {
 	for row := 0; row < 3; row++ {
 		for col := 0; col < 3; col++ {
 			if grid[row+boxStartRow][col+boxStartCol] == num {
@@ -80,7 +81,7 @@ func usedInBox(grid [N][N]int, boxStartRow, boxStartCol, num int) bool {
 }
 
 // printGrid prints the grid
-func printGrid(grid [N][N]int) {
+func printGrid(grid [][]int) {
 	for row := 0; row < N; row++ {
 		for col := 0; col < N; col++ {
 			fmt.Printf("%2d", grid[row][col])
@@ -91,7 +92,7 @@ func printGrid(grid [N][N]int) {
 
 func main() {
 	// Example puzzle
-	grid := [N][N]int{
+	grid := [][]int{
 		{5, 3, 0, 0, 7, 0, 0, 0, 0},
 		{6, 0, 0, 1, 9, 5, 0, 0, 0},
 		{0, 9, 8, 0, 0, 0, 0, 6, 0},
@@ -103,9 +104,6 @@ func main() {
 		{0, 0, 0, 0, 8, 0, 0, 7, 9},
 	}
 
-	if SolveSudoku(grid) {
-		printGrid(grid)
-	} else {
-		fmt.Println("No solution exists")
-	}
+	grid = SolveSudoku(grid)
+	printGrid(grid)
 }
