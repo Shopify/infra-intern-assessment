953008f93b3b4c932894a381f112f80b86751904
Jessica Li <61724009+Li-Jessica@users.noreply.github.com>
2024-01-27 23:28:52 -0500

    Update comments format

--- sudoku.go
+++ sudoku.go
@@ -3,7 +3,7 @@ package main
 // Function Name: 	SolveSudoku
 // Purpose: 		The main function used to solve a given sudoku board
 // Parameters:		board - 2D integer array representing the sudoku board
-// Returns:			[][]int
+// Returns:		[][]int
 func SolveSudoku(board [][]int) [][]int {
 	// Get the dimensions of the board
 	rowLength := len(board[0])
@@ -27,13 +27,13 @@ func SolveSudoku(board [][]int) [][]int {
 
 // Function Name: 	findSolution
 // Purpose: 		Recursively attempts to solve the board by finding the next number to fill
-//					Backtracks as needed if the filled number does not satisfy the game's restrictions
+//			Backtracks as needed if the filled number does not satisfy the game's restrictions
 //
-// Parameters:		board 		   - the current state of the sudoku board
-//					zeroesPosition - 2D integer array representing coordinates (row, column) of the empty spots to be filled
-//					position 	   - the current position
+// Parameters:		board		 - the current state of the sudoku board
+//			zeroesPosition   - 2D integer array representing coordinates (row, column) of the empty spots to be filled
+//			position 	 - the current position
 //
-// Returns:			[][]int
+// Returns:		[][]int
 func findSolution(board [][]int, zeroesPosition [][]int, position int) [][]int {
 	// Base case: return if all the zeroes spots are filled
 	if position == len(zeroesPosition) {
@@ -64,12 +64,12 @@ func findSolution(board [][]int, zeroesPosition [][]int, position int) [][]int {
 }
 
 // Function name:	isInSquare
-// Purpose:			Checks if given number is present in the 3x3 sub-grid of the sudoku board
+// Purpose:		Checks if given number is present in the 3x3 sub-grid of the sudoku board
 //
 // Parameters:		board - the current sudoku board
-//					row   - the row index of the current cell
-//					col   - the column index of the current cell
-//					num   - the number to check for
+//			row   - the row index of the current cell
+//			col   - the column index of the current cell
+//			num   - the number to check for
 //
 // Returns: 		boolean
 func isInSquare(board [][]int, row, col, num int) bool {
@@ -85,15 +85,14 @@ func isInSquare(board [][]int, row, col, num int) bool {
 }
 
 // Function name: 	checkValidity
-// Purpose:			Checks the validity of placing the number in a specific spot
-//					against the current row, current column, and current 3x3 subgrid
+// Purpose:		Checks the validity of placing the number in a specific spot against the current row, current column, and current 3x3 subgrid
 //
 // Parameters:		board - the current sudoku board
-//					row   - the row index of the current cell
-//					col   - the column index of the current cell
-//					num   - the number to check for
+//			row   - the row index of the current cell
+//			col   - the column index of the current cell
+//			num   - the number to check for
 //
-// Returns:			boolean
+// Returns:		boolean
 func checkValidity(board [][]int, row int, col int, num int) bool {
 	for i := 0; i < 9; i++ {
 		if board[row][i] == num || board[i][col] == num || isInSquare(board, row, col, num) {
