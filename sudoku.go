2fd81b9c723cf273ebd38601821ac9bde5f76bcf
Mohamed-Grira <hammouda.grira22@gmail.com>
2024-01-28 16:15:12 -0500

    add helper function findEmptyCell

--- sudoku.go
+++ sudoku.go
@@ -1,5 +1,20 @@
 package main
 
+const gridSize = 9 // the grid size is 9x9 and does not change
+
 func SolveSudoku(grid [][]int) [][]int {
 	return grid
+}
+
+// findEmptyCell is a helper function that returns the position of the cell if it contains contains 0
+// meaning that the cell is empty
+func findEmptyCell(grid [][]int) []int {
+    for row := 0; row < gridSize; row++ {
+        for col := 0; col < gridSize; col++ {
+            if grid[row][col] == 0 {
+                return []int{row, col}
+            }
+        }
+    }
+    return nil
 }
