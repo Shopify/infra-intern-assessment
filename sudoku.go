caec0330d9b9c348fa562cf1915bcfeb7cccb6e9
Mohamed-Grira <hammouda.grira22@gmail.com>
2024-01-28 16:17:02 -0500

    add the code logic for solving sudoku

--- sudoku.go
+++ sudoku.go
@@ -3,7 +3,35 @@ package main
 const gridSize = 9 // the grid size is 9x9 and does not change
 
 func SolveSudoku(grid [][]int) [][]int {
+	solveSudokuHelper(grid)
 	return grid
+}
+
+// solveSudokuHelper is a helper function that updates the input grid recursively
+func solveSudokuHelper(grid [][]int) bool {
+    emptyCell := findEmptyCell(grid)
+    if emptyCell == nil {
+        return true
+    }
+
+	// row and column of the empty cell
+    row, col := emptyCell[0], emptyCell[1]
+
+	// check all the number possibilities from 1..9
+    for num := 1; num <= gridSize; num++ {
+        if isValidMove(grid, num, []int{row, col}) {
+            grid[row][col] = num // put the new number in the grid
+
+            if solveSudokuHelper(grid) {
+                return true
+            }
+
+			// backtrack if the number is not valid, and empty the cell
+            grid[row][col] = 0
+        }
+    }
+
+    return false
 }
 
 // isValidMove is a helper function that checks if the new added number is valid in the row, column, and sub-grid
@@ -16,7 +44,7 @@ func isValidRow(grid [][]int, num int, pos []int) bool {
 	// row and column of the current number
 	numRow, numCol := pos[0], pos[1]
 
-    for col := 0; col< gridSize; col++ {
+    for col := 0; col < gridSize; col++ {
         if grid[numRow][col] == num && numCol != col {
             return false
         }
@@ -24,7 +52,7 @@ func isValidRow(grid [][]int, num int, pos []int) bool {
     return true
 }
 
-// isValidColumn is a helper function that checks if the add number is a valid entry in the colum
+// isValidColumn is a helper function that checks if the add number is a valid entry in the column
 func isValidColumn(grid [][]int, num int, pos []int) bool {
 	// row and column of the current number
 	numRow, numCol := pos[0], pos[1]
@@ -55,7 +83,7 @@ func isValidSubGrid(grid [][]int, num int, pos []int) bool {
     return true
 }
 
-// findEmptyCell is a helper function that returns the position of the cell if it contains contains 0
+// findEmptyCell is a helper function that returns the position of the cell if it contains 0
 // meaning that the cell is empty
 func findEmptyCell(grid [][]int) []int {
     for row := 0; row < gridSize; row++ {
