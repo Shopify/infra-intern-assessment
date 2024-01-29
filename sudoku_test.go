2dbfac95e32e940abd2b685a515f8bc88c52cdd4
K B <superargaze@gmail.com>
2024-01-29 01:26:50 -0500

    Extra test case

--- sudoku_test.go
+++ sudoku_test.go
@@ -35,4 +35,39 @@ func TestSolveSudoku(t *testing.T) {
 	if !reflect.DeepEqual(solved, expected) {
 		t.Errorf("Sudoku puzzle was not solved correctly. Expected:\n%v\n\nGot:\n%v", expected, solved)
 	}
+
+	// From sudoku book
+	/*
+	   func TestSolveSudoku(t *testing.T) {
+	   	input := [][]int{
+	   		{0, 0, 6, 0, 0, 0, 5, 0, 8},
+	   		{1, 0, 2, 3, 8, 0, 0, 0, 4},
+	   		{0, 0, 0, 2, 0, 0, 1, 9, 0},
+	   		{0, 0, 0, 0, 6, 3, 0, 4, 5},
+	   		{0, 6, 3, 4, 0, 5, 8, 7, 0},
+	   		{5, 8, 0, 9, 2, 0, 0, 0, 0},
+	   		{0, 4, 7, 0, 0, 4, 0, 0, 0},
+	   		{2, 0, 0, 0, 9, 8, 4, 0, 7},
+	   		{4, 0, 9, 0, 0, 0, 3, 0, 0},
+	   	}
+
+	   	expected := [][]int{
+	   		{3, 9, 6, 7, 4, 1, 5, 2, 8},
+	   		{1, 5, 2, 3, 8, 9, 7, 6, 4},
+	   		{8, 7, 4, 2, 5, 6, 1, 9, 3},
+	   		{7, 2, 1, 8, 6, 3, 9, 4, 5},
+	   		{9, 6, 3, 4, 1, 5, 8, 7, 2},
+	   		{5, 4, 8, 9, 2, 7, 6, 3, 1},
+	   		{6, 8, 7, 1, 3, 4, 2, 5, 9},
+	   		{2, 3, 5, 6, 9, 8, 4, 1, 7},
+	   		{4, 1, 9, 5, 7, 2, 3, 8, 6},
+	   	}
+
+	   	solved := SolveSudoku(input)
+
+	   	if !reflect.DeepEqual(solved, expected) {
+	   		t.Errorf("Sudoku puzzle was not solved correctly. Expected:\n%v\n\nGot:\n%v", expected, solved)
+	   	}
+
+	*/
 }
