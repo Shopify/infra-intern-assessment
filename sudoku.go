c7e1efc31cad17ede1bfc7a6c30801490f1bcb53
Sheldon Dacon <54724209+SheldonDacon@users.noreply.github.com>
2024-01-28 22:46:16 -0500

    grammar update sudoku.go
    Final update, did a grammar check

--- sudoku.go
+++ sudoku.go
@@ -2,7 +2,7 @@ package main
 
 // - Here is my final solution for the Shopifly Technical Challenge
 // - I created a backtracking algorithm to create the sudoku solver.
-// - My solution has three functions. The first function (SolveSudoku simply calls the main helper function
+// - My solution has three functions. The first function (SolveSudoku) simply calls the main helper function
 // which is called solve_sudoku on the grid. solve_sudoku is the main recursive function that does the solving
 // and backtracking.
 // - Lastly solve_sudoku often calls is_legal to check if the number to
@@ -22,29 +22,29 @@ func SolveSudoku(grid [][]int) [][]int {
 
 // - This is solve_sudoku
 // - This is a recursive function that uses backtracking.
-// - The first three if or else if statements check the general location of the cell
+// - The first three if and else if statements check the general location of the cell
 // - The input is a 9x9 sudoku grid and the exact row and column to start at
-// - It solves from left the right at the top left corner (grid[0][0] is the top left)
+// - It solves from left the right hgoing down the grid.
 // - It returns true if the row is equal to 9. This is because the indexing starts at
 // 0 so if the pointer reaches row 9 it means it has already checked all of row 8 which is
 // the last row.
 // - The same idea applies to columns, if it reaches column 9 then we know that that entire row is checked
 // and we can go to start the next row which is grid[row + 1][0] and recursively call the function at that location
-// If the row and column inputs are valid it then checks if it has been filled (i.e doesn't contain 0)
-// If it has been filled it moves on until it finds a cell with a zero by recursively calling the function on the adjacent cell.
+// - If the row and column inputs are valid it then checks if it has been filled (i.e doesn't contain 0)
+// - If it has been filled it moves on until it finds a cell with a zero by recursively calling the function on the adjacent cell.
 
-// The last else statement is where the backtracking happens
-// if we reach the else block this means that at this location there is a zero
-//The function then iterates through all possible numbers 1-9 then calls isLegal using the the coordinates and
-// The number to see if you can put the number there without breaking the rules
-//If it can't find a suitable number it returns false
-//If it can find one, it fills that cell with that number and recursively calls the function again
-// in the adjacent block. it then resources through the grid, if it runs into a case where it can't find a suitable
+// - The last else statement is where the backtracking happens
+// - If we reach the else block, this means that at this location there is a zero
+// - The function then iterates through all possible numbers 1-9 then calls isLegal using the the coordinates and
+// a number from 1-9 to see if you can put the number there without breaking the rules
+//- If it can't find a suitable number it returns false
+//- If it can find one, it fills that cell with that number and recursively calls the function again
+// in the adjacent block. It then recurses through the grid, if it runs into a case where it can't find a suitable
 // number it would backtrack and set the spot to 0.
 
 func solve_sudoku(grid [][]int, r, c int) bool {
 	if r == 9 {
-		return true //  function has checked entire grid and can return true
+		return true // function has checked entire grid and can return true
 
 	} else if c == 9 {
 		return solve_sudoku(grid, r+1, 0) //  function has checked the entire row and can go to the next row
@@ -70,17 +70,17 @@ func solve_sudoku(grid [][]int, r, c int) bool {
 // This is the is_legal function
 // - It returns a boolean value on whether adding a number to a cell at the given coordinates is legal.
 // - This means that each row, column, and sub-grid contains all the numbers from 1-9 without repetition.
-// This function first checks if the given number is in the same row (all values in grid[row]).
-// It then checks if the given number is in the same column (all values in grid[i][col] for a 0<= i <9).
-// Lastly it checks if the number is in the same 3 by 3 grid we do this by calucating the start and end coordinates of the grid
+// - This function first checks if the given number is in the same row (all values in grid[row]).
+// - It then checks if the given number is in the same column (all values in grid[i][col] for a 0<= i <9).
+// - Lastly, it checks if the number is in the same 3 by 3 grid we do this by calucating the start and end coordinates of the grid
 // They are 3*(row/ floor(3)) and 3*(col/floor(3)
-// we then check all values in range 3 * (row/ floor(3)) to (3 * (row/ floor(3))) +3
+// - We then check all values in range 3 * (row/ floor(3)) to (3 * (row/ floor(3))) +3
 // and 3 * col/floor(3) to (3 * (col/floor(3))) + 3, we must check all cells in that range. There are 9 in total.
 
 func is_legal(grid [][]int, row, col, value int) bool {
 
 	for _, num := range grid[row] {
-		if num == value { //checks if the given number is in the same row
+		if num == value { // checks if the given number is in the same row
 			return false // The attempted move is not allowed
 		}
 	}
@@ -102,4 +102,3 @@ func is_legal(grid [][]int, row, col, value int) bool {
 
 	return true // the cell with selected number is allowed
 }
-
