08cc1482cdf10d8256ef5a72e33452550dddc248
6MB11 <45149917+6MB11@users.noreply.github.com>
2024-01-29 00:49:21 -0500

    Update sudoku.go
    Adding comments for functions and correcting spelling in comments

--- sudoku.go
+++ sudoku.go
@@ -1,5 +1,6 @@
 package main
 
+// function to create initial arrays to represent avaliable locations for candidates
 func SolveSudoku(sudoku [][]int) [][]int {
 
 	// 2D arrays, in ith, jth location, store 1 if number j+1 is present in ith row, otherwise store 0
@@ -63,6 +64,7 @@ func SolveSudoku(sudoku [][]int) [][]int {
 	return sudoku
 }
 
+// function to recursively trial candidates
 func sudokuSolverCore(sudoku [][]int, PresentInRow [][]int, PresentInColumn [][]int, PresentInBox [][]int,
 	zeroCounter int, iStart int, jStart int) ([][]int, int) {
 
@@ -86,14 +88,14 @@ func sudokuSolverCore(sudoku [][]int, PresentInRow [][]int, PresentInColumn [][]
 						PresentInBox[boxId][numToTry-1] = 1
 						zeroCounter -= 1
 
-						// make recrusive call to go to next location and try out all valid digits there
+						// make recursive call to go to next location and try out all valid digits there
 						if j == 8 {
 							sudoku, zeroCounter = sudokuSolverCore(sudoku, PresentInRow, PresentInColumn, PresentInBox, zeroCounter, i+1, 0)
 						} else {
 							sudoku, zeroCounter = sudokuSolverCore(sudoku, PresentInRow, PresentInColumn, PresentInBox, zeroCounter, i, j+1)
 						}
 
-						// revert to previous solution if recrusive calls did not find a solution for the current setup
+						// revert to previous solution if recursive calls did not find a solution for the current setup
 						if zeroCounter != 0 {
 							sudoku[i][j] = 0
 							zeroCounter += 1
