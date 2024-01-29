8dafef0e9659d18f7310823739a9e6535812361d
K B <superargaze@gmail.com>
2024-01-29 01:01:53 -0500

    Sudoku Solution

--- sudoku.go
+++ sudoku.go
@@ -12,7 +12,7 @@ func solve(board [][]int) bool { // Solves the sudoku board using backtracking a
 	for row := 0; row < 9; row++ {
 		for col := 0; col < 9; col++ {
 			if board[row][col] == 0 {
-				for num := 1; num <= 9; num++ {
+				for num := 1; num <= 9; num++ { // try numbers from 1 to 9
 					if isValid(board, row, col, num) {
 						board[row][col] = num
 						if solve(board) {
@@ -21,15 +21,15 @@ func solve(board [][]int) bool { // Solves the sudoku board using backtracking a
 						board[row][col] = 0
 					}
 				}
-				return false
+				return false // if no number gives solution
 			}
 		}
 	}
-	return true
+	return true // when the grid is complete
 }
 
 // helper function
-func isValid(board [][]int, row, col, num int) bool { //Checks whether the number is present in column row or sub-grid
+func isValid(board [][]int, row, col, num int) bool { //Checks whether the number is present in column row or 3x3 sub-grid
 	for i := 0; i < 9; i++ {
 		if board[i][col] == num {
 			return false
