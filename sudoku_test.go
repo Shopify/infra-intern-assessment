2c1642687304716977a098ead308c25d268629a3
Carmelo Restivo <carmrest00@gmail.com>
2024-01-29 22:10:35 -0500

    Added some test cases

--- sudoku_test.go
+++ sudoku_test.go
@@ -36,3 +36,70 @@ func TestSolveSudoku(t *testing.T) {
 		t.Errorf("Sudoku puzzle was not solved correctly. Expected:\n%v\n\nGot:\n%v", expected, solved)
 	}
 }
+
+func TestSolveHardSudoku(t *testing.T) {
+	input := [][]int{
+		{0, 1, 6, 0, 0, 3, 0, 0, 7},
+		{0, 0, 4, 6, 7, 0, 3, 0, 0},
+		{9, 3, 0, 5, 0, 0, 0, 8, 0},
+		{6, 0, 0, 0, 0, 8, 0, 3, 5},
+		{3, 7, 0, 0, 6, 0, 8, 0, 0},
+		{0, 5, 8, 0, 0, 7, 0, 0, 6},
+		{0, 8, 5, 7, 0, 0, 0, 6, 0},
+		{0, 6, 0, 0, 3, 0, 5, 0, 0},
+		{7, 0, 3, 0, 0, 0, 0, 4, 0},
+	}
+
+	expected := [][]int{
+		{5, 1, 6, 9, 8, 3, 4, 2, 7},
+		{8, 2, 4, 6, 7, 1, 3, 5, 9},
+		{9, 3, 7, 5, 2, 4, 6, 8, 1},
+		{6, 4, 2, 1, 9, 8, 7, 3, 5},
+		{3, 7, 9, 2, 6, 5, 8, 1, 4},
+		{1, 5, 8, 3, 4, 7, 2, 9, 6},
+		{4, 8, 5, 7, 1, 2, 9, 6, 3},
+		{2, 6, 1, 4, 3, 9, 5, 7, 8},
+		{7, 9, 3, 8, 5, 6, 1, 4, 2},
+	}
+
+
+
+	solved := SolveSudoku(input)
+
+	if !reflect.DeepEqual(solved, expected) {
+		t.Errorf("Sudoku puzzle was not solved correctly. Expected:\n%v\n\nGot:\n%v", expected, solved)
+	}
+}
+
+func TestSolved(t *testing.T) {
+
+    input := [][]int{
+		{5, 1, 6, 9, 8, 3, 4, 2, 7},
+		{8, 2, 4, 6, 7, 1, 3, 5, 9},
+		{9, 3, 7, 5, 2, 4, 6, 8, 1},
+		{6, 4, 2, 1, 9, 8, 7, 3, 5},
+		{3, 7, 9, 2, 6, 5, 8, 1, 4},
+		{1, 5, 8, 3, 4, 7, 2, 9, 6},
+		{4, 8, 5, 7, 1, 2, 9, 6, 3},
+		{2, 6, 1, 4, 3, 9, 5, 7, 8},
+		{7, 9, 3, 8, 5, 6, 1, 4, 2},
+	}
+
+	expected := [][]int{
+		{5, 1, 6, 9, 8, 3, 4, 2, 7},
+		{8, 2, 4, 6, 7, 1, 3, 5, 9},
+		{9, 3, 7, 5, 2, 4, 6, 8, 1},
+		{6, 4, 2, 1, 9, 8, 7, 3, 5},
+		{3, 7, 9, 2, 6, 5, 8, 1, 4},
+		{1, 5, 8, 3, 4, 7, 2, 9, 6},
+		{4, 8, 5, 7, 1, 2, 9, 6, 3},
+		{2, 6, 1, 4, 3, 9, 5, 7, 8},
+		{7, 9, 3, 8, 5, 6, 1, 4, 2},
+	}
+
+	solved := SolveSudoku(input)
+
+	if !reflect.DeepEqual(solved, expected) {
+		t.Errorf("Sudoku puzzle was not solved correctly. Expected:\n%v\n\nGot:\n%v", expected, solved)
+	}
+}
